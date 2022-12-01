cf6499a141b237f7ed8cd1110fb5a059c44e9b15
Mauricio Heller <hellerdejanuar@gmail.com>
2022-12-01 00:47:18 -0300

    Getting to know tailwind, reformatting old stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :users
   get 'errors/not_found'
   get 'home/index'
   get 'index', to:'home#index'
   get 'home/not_available'
   get 'home/about'
+
   
   match "/404", to: "errors#not_found", via: :all # error handling
   match "/500", to: "errors#internal_server_error", via: :all # to change
